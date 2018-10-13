df7c4eb4571ece91347297ef67108f9cb2b734e6
Baptiste <vrignaudbaptiste@gmail.com>
2018-10-13 14:08:26 +0200

    refactor: mapping servlet in web.xml

--- src/main/java/servlet/SensorPub.java
+++ src/main/java/servlet/SensorPub.java
@@ -4,7 +4,6 @@ import mqtt.MqttClientManager;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,7 +12,6 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Enumeration;
 
-@WebServlet("/sensor")
 public class SensorPub extends HttpServlet {
 
     @Override
